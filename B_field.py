0130cf9db6a53f924587b569b3c2bbe2d2e620f9
phenix-dream <saladinifabien2@gmail.com>
2025-10-31 16:05:49 +0100

    t

--- B_field.py
+++ B_field.py
@@ -6,10 +6,16 @@ class MagneticField:
     def __init__(self, k, zc,r,theta,z ):
         self.k = k
         self.zc = zc
+
         self.r = r
         self.theta = theta
         self.z = z
-        self.R,self.Theta,self.Z=np.meshgrid(r,theta,z,indexing='ij')
+        self.R,self.Theta,self.Z=np.meshgrid(self.r,self.theta,self.z,indexing='ij')
+        # self.x= self.r * np.cos(self.theta)
+        # self.y= self.r * np.sin(self.theta)
+        
+        # print(self.x,self.y)
+        # quit()
 
         
     def B_field_lines(self,Z,R,k,zc,smooth=300):
@@ -22,7 +28,10 @@ class MagneticField:
   
 
     def Compute_field(self):    
+
+    
         B=self.B_field_lines(self.Z,self.R,self.k,self.zc)
+   
 
 
         Bz,Bt,Br=np.gradient(B,self.r,self.theta,self.z)
@@ -35,9 +44,14 @@ class MagneticField:
         Bz/=magnitude
         Bt/=magnitude
 
+        Bz[:Br.shape[0]//2,:,:]*=-1  #inverser le sens du champ magnétique dans la moitié inférieure
+        Br[:Br.shape[0]//2,:,:]*=-1  #inverser le sens du champ magnétique dans la moitié inférieure
+
         self.Br=Br
         self.Bt=Bt
         self.Bz=Bz
+
+
 
         #passage en cartésien
         X=self.R * np.cos(self.Theta)
@@ -48,6 +62,7 @@ class MagneticField:
 
         Bx = Br * np.cos(self.Theta) - Bt * np.sin(self.Theta)
         By = Br * np.sin(self.Theta) + Bt * np.cos(self.Theta)
+
 
         self.Bx=Bx
         self.By=By
@@ -84,32 +99,56 @@ class MagneticField:
 def main():
 
     z=np.linspace(-2000,2000,25)
-    r=np.linspace(0.1,1500,15)
-    theta=np.linspace(0,2*np.pi,4)
+    # r=np.linspace(0.1,1500,15)
+    # theta=np.linspace(0,2*np.pi,4)
+
+    x=np.linspace(-1500,1500,10)
+    y=np.linspace(-1500,1500,10)
 
     k=0.01
     zc=1000
 
-    magnetic_field=MagneticField(k, zc,r,theta,z)
+    # magnetic_field=MagneticField(k, zc,r,theta,z)
+    magnetic_field=MagneticField(k, zc,x,y,z)
 
-    R=magnetic_field.R
+    # R=magnetic_field.R
     Bx,By,Bz,X,Y,Z=magnetic_field.Compute_field()
-    Br=magnetic_field.Br
-    gradient_magnitude=magnetic_field.Compute_gradient()
+    # Br=magnetic_field.Br
+    # gradient_magnitude=magnetic_field.Compute_gradient()
 
-    fig=plt.figure(figsize=(40,40))
+    x=np.unique(X)
+    y=np.unique(Y)
+    z=np.unique(Z)
+
+    print(x.shape,y.shape,z.shape)
+    print(Bx.shape,By.shape,Bz.shape)
+
+    e=1
+
+    fig=plt.figure(figsize=(20,20))
     ax=fig.add_subplot(111, projection='3d')
-    ax.quiver(X,Y,Z,Bx,By,Bz,color='black',label='Magnetic Field Vectors',length=100,linewidth=2,arrow_length_ratio=0.5,normalize=True)
+    # ax.quiver(X[:,e,:],Y[:,e,:],Z[:,e,:],Bx[:,e,:],By[:,e,:],Bz[:,e,:],color='black',label='Magnetic Field Vectors',length=100,linewidth=2,arrow_length_ratio=0.5,normalize=True)
+    ax.quiver(X[:,:,:],Y[:,:,:],Z[:,:,:],Bx[:,:,:],By[:,:,:],Bz[:,:,:],color='black',label='Magnetic Field Vectors',length=100,linewidth=2,arrow_length_ratio=0.5,normalize=True)
     plt.xlabel('x')
     plt.ylabel('y')
     ax.set_zlabel('z')
     # plt.show()
 
-
     plt.figure()
-    plt.pcolormesh(Z[:,0,:],R[:,0,:],gradient_magnitude[:,0,:],cmap='viridis')
+    # plt.pcolormesh(Z[:,e,:],X[:,e,:]gradient_magnitude[:,e,:],cmap='viridis')
+    # plt.colorbar(label='|grad(B)|')
+    plt.quiver(Z[:,e,:],X[:,e,:],Bz[:,e,:],Bx[:,e,:],color='white',label='B',width=0.003,scale=50,headwidth=3)
+    plt.xlabel('z')
+    plt.ylabel('x')
+    plt.title('Magnetic Field and its gradient in Magnetic Mirror Configuration')
+    plt.legend()
+    # plt.savefig('B_field_mirror.png')
+    plt.show()
+    quit()
+    plt.figure()
+    plt.pcolormesh(Z[:,e,:],R[:,e,:],gradient_magnitude[:,e,:],cmap='viridis')
     plt.colorbar(label='|grad(B)|')
-    plt.quiver(Z[:,0,:],R[:,0,:],Bz[:,0,:],Br[:,0,:],color='white',label='B',width=0.003,scale=50,headwidth=3)
+    plt.quiver(Z[:,e,:],R[:,e,:],Bz[:,e,:],Br[:,e,:],color='white',label='B',width=0.003,scale=50,headwidth=3)
     plt.xlabel('z')
     plt.ylabel('r')
     plt.title('Magnetic Field and its gradient in Magnetic Mirror Configuration')
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from plot import plot_trajectory, plot_compare, plot_errors
+from plot import plot_trajectory, plot_compare, plot_errors,plot_trajectory_3d
 from plasma import cyclotron_xy, ExB_xy
 from B_field import MagneticField
 
@@ -15,13 +15,13 @@ from B_field import MagneticField
 
 def magnetic_field(x, t,B0,B0_space):
     """Return B(x,t) as a numpy array"""
+    distances = np.sqrt((B0_space[0] - x[0])**2 + (B0_space[1] - x[1])**2 + (B0_space[2] - x[2])**2)
+    flat_index = np.argmin(distances)
 
-    #interpolation of the magnetic field values
-    Bx = np.interp(x[0], B0_space[0], B0[0][:,0,0])
-    By = np.interp(x[1], B0_space[1], B0[1][0,:,0])
-    Bz = np.interp(x[2], B0_space[2], B0[2][0,0,:])
+    # Convert the flat index to 3D indices
+    i, j, k = np.unravel_index(flat_index, B0_space[0].shape)
 
-    return np.array([Bx, By, Bz])
+    return np.array([B0[0][i,j,k], B0[1][i,j,k], B0[2][i,j,k]])
 # -------------------------------------------------
 # Boris pusher
 # -------------------------------------------------
@@ -99,36 +99,45 @@ if __name__ == "__main__":
     omega = q*B0/m
     T=2*np.pi/abs(omega)
     dt = 0.1
-    NT = 2
+    NT = 100
     nsteps = int(NT*T/dt)
 
-    x0 = np.array([1.0, 0.0, 0.0])
-    v0 = np.array([0.0, 1.0, 0.0])
+    x0 = np.array([0.0,0.0, 500.0])
+    v0 = np.array([0.0, 100.0, 50.0])
+    # v0 = np.array([0.0, 10.0, 100.0])
 
-    E0 = np.array([0.0, 1.0, 0.0])
-    B0 = np.array([0.0, 0.0, 1.0])
+    # x0=np.array([0
 
-    magnetic_field=MagneticField(k=0.1, zc=1000, r=np.linspace(0.1,1500,15), theta=np.linspace(0,2*np.pi,36), z=np.linspace(-2000,2000,25))
-    Bx,By,Bz,X,Y,Z=magnetic_field.Compute_field()
+    # E0 = np.array([0.0, 1.0, 0.0])
+    # B0 = np.array([0.0, 0.0, 1.0])
+
+    B_field=MagneticField(k=0.1, zc=1000, r=np.linspace(0.1,1500,15), theta=np.linspace(0,2*np.pi,36), z=np.linspace(-2000,2000,25))
+    Bx,By,Bz,X,Y,Z=B_field.Compute_field()
     B0=[Bx,By,Bz]
-    x=X[:,0,0]
-    y=Y[0,:,0]
-    z=Z[0,0,:]
-    B0_space=[x,y,z]
+    B0_space=[X,Y,Z]
 
     E0=np.array([0.0, 0.0, 0.0])
+    # B0=np.zeros_like(B0)
    
 
-    # Numerical trajectory
+
+    # NumericalS trajectory
     xs_num, vs_num = simulate(x0, v0, q, m, dt, nsteps,B0,E0,B0_space)
-    
-    
-    # plot_trajectory(xs_num, vs_num, step=5)
+    print(xs_num)
+    plt.figure()
+    plt.plot(xs_num[:,2],xs_num[:,1])
+    plt.xlabel('z')
+    plt.ylabel('y')
+    plt.show()
+
+    quit()
+    plot_trajectory(xs_num, vs_num, step=5,axis=[2,1])
+    plot_trajectory_3d(xs_num, vs_num, step=5)
 
     # # Analytical trajectory : cyclotron motion
     times = np.arange(0, NT*T, dt) + dt/2
     # x_cyc, v_cyc = cyclotron_xy(x0, v0, q, m, B0[2], times)
-
+    
     x_ExB, v_ExB = ExB_xy(x0, v0, q, m, E0, B0, times)
 
     # print(vs_num[0,:], v_ExB[0,:])
@@ -138,9 +147,9 @@ if __name__ == "__main__":
     rms = np.sqrt(np.mean(np.sum(diff**2, axis=1)))
     print(f"RMS position error (xy): {rms:.3e}")
 
-
     # Plots to compare
     plot_compare(xs_num, vs_num, x_ExB, v_ExB, step=8, show_arrows=True)
     plot_errors(times, diff)
+    
 
 
